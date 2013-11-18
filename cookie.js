80e3be57349673a986cc8dca7dd739e9ed869454
godxiaoji <godxiaoji@vip.qq.com>
2013-11-18 15:48:14 +0800

    代码优化

--- cookie.js
+++ cookie.js
@@ -1,46 +1,57 @@
 /**
- * @Name    cookie.js
- * @Author  linyongji
- * @Version 1.0.1
+ * cookie.js
+ * @Author  Travis(LinYongji)
+ * @Contact http://travisup.com/
+ * @Version 1.0.2
+ * @date    2013-10-25
  */
-var cookie = function(name, value, options) {
-    if(typeof name === "undefined") return null;
-    // get
-    if(typeof value === "undefined") {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != "") {
-            var cookies = document.cookie.split("; ");
-            for (var i = 0; i < cookies.length; i++) {
-                if(cookies[i].indexOf(name+"=") === 0) {
-                    cookieValue = decodeURIComponent(cookies[i].substr(name.length+1));
+var cookie = function( name, value, options ) {
+    var i, len,
+        cookies, ret, date,
+        expires, path, secure;
+    
+    // 没传入参数返回，不开放获取全部cookie
+    if ( name == null ) {
+        return ret;
+    }
+    // 读取cookie
+    if ( typeof value === "undefined" ) {
+        if ( document.cookie && document.cookie != "" ) {
+            cookies = document.cookie.split("; ");
+            for ( i = 0, len = cookies.length; i < len; i++ ) {
+                if ( cookies[i].indexOf( name + "=" ) === 0 ) {
+                    ret = decodeURIComponent( cookies[i].substr( name.length + 1 ) );
                     break;
                 }
             }
         }
-        return cookieValue;
+        // 没找到返回undefined
+        return ret;
     }
-    // set
+    // 设置cookie
     options = options || {};
-    // delete
-    if (value === null) {
+    // 设置为删除cookie
+    if ( value === null ) {
         value = "";
         options.expires = -1;
     }
-    var expires = "";
-    if (options.expires && (typeof options.expires == "number" || options.expires.toUTCString)) {
-        var date;
-        if (typeof options.expires == "number") {
+    // 设置过期时间(支持传入秒和Date类型)
+    expires = "";
+    if ( options.expires && ( typeof options.expires === "number" || options.expires.toUTCString ) ) {
+        if ( typeof options.expires === "number" ) {
             date = new Date();
-            date.setTime(date.getTime() + options.expires * 1000);
+            date.setTime( date.getTime() + options.expires * 1000 );
         } else {
             date = options.expires;
         }
         expires = "; expires=" + date.toUTCString();
     }
-    var path = options.path ? "; path=" + options.path : "";
-    var domain = options.domain ? "; domain=" + options.domain : "";
-    var secure = options.secure ? "; secure" : "";
-    var ret = [name, "=", encodeURIComponent(value), expires, path, domain, secure].join("");
+    // 设置域，安全性等
+    path = options.path ? "; path=" + options.path : "";
+    domain = options.domain ? "; domain=" + options.domain : "";
+    secure = options.secure ? "; secure" : "";
+    // 写入cookie
+    ret = [ name, "=", encodeURIComponent( value ), expires, path, domain, secure ].join("");
     document.cookie = ret;
     return ret;
 };
